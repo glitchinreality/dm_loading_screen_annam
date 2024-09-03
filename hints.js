b56bf2e051425592733d63956df1b00fb5dd4548
glitchinreality <42478782+glitchinreality@users.noreply.github.com>
2024-09-03 17:48:39 -0500

    Update hints.js
    Added a few extra hints, including some homebrew and setting specific stuff

--- hints.js
+++ hints.js
@@ -10,12 +10,19 @@ export default
 		"Murder is frowned upon in most civilized locations.",
 		"Remember to add your proficiency bonus to your attack roll!",
 		"This is not Skyrim! Your words may end a fight just as efficiently as your blades.",
-		"Running away is almost always an option. Call it tactical retreat to save some face."
+		"Running away is almost always an option. Call it tactical retreat to save some face.",
+	    	"Half cover grants a +2 to AC and dexterity saves. People count as half cover!",
+	    	"Three-Quarters cover gives a +5 to ac and dexterity saves.",
+	    	"If someone is heavily obscured or behind full cover, you probably can't cast a spell on them.",
+	    	"Flanking gives a +2 to your attack."
     ],
 	"social": [
 		"Depending on your location, people may be open to talking to you.",
 		"Talk to your fellow party members. This is a role playing game, after all.",
-		"Persuasion is not mind control. No, not even if you roll a 20."
+		"Persuasion is not mind control. No, not even if you roll a 20.",
+		"Not everything attacking you wants you dead.",
+		"A parley may work when you least expect it!",
+		"You don't always have to go for the kill. A duel to first blood is just as respectable."
 	],
     "default": [
     	"Bribing your DM with their preferred beverage may result in inspiration and/or inebriation.",
@@ -27,6 +34,7 @@ export default
 		"You can extend your arms half your height above yourself during a high jump to reach ledges.",
 		"If it consists of pentagons, it's a d12 not a d20.",
 		"Beware of the smiling DM!",
-		"Every time a dragon crits, a PC gets their wings."
+		"Every time a dragon crits, a PC gets their wings.",
+	    	"South of the mountains, the Orc kingdom prepares their assault. Or at least thats what Oliver says..."
 	]
 }
